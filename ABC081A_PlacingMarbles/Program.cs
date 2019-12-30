bdb6a16292b74a7946ace88bd79486d65607d5f5
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2019-12-31 08:30:17 +0900

    ABC081A solved

---
+++ ABC081A_PlacingMarbles/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+namespace ABC081A_PlacingMarbles
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input.ToCharArray().Where(e => e == '1').Count());
+        }
+    }
+}
