f8debfd789e3b34936574d643999bec1bb205334
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2019-12-31 08:07:43 +0900

    ABC086A solved

---
+++ ABC086A_Product/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ABC086A_Product
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            Console.WriteLine((a * b) % 2 == 0 ? "Even" : "Odd");
+        }
+    }
+}
