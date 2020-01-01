24a04603d45def78f1239b922f359d6cf34c9006
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-02 08:15:05 +0900

    ABC081_B solved

---
+++ ABC081B_ShiftOnly/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+namespace ABC081B_ShiftOnly
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] inp = Console.ReadLine().Split(' ').Select<string, int>(e => int.Parse(e)).ToArray();
+            int cnt = 0;
+            bool flag = true;
+            while (flag)
+            {
+                if(inp.Any(e => e%2 != 0))
+                {
+                    break;
+                }
+                inp = inp.Select(e => e / 2).ToArray();
+
+                cnt++;
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
