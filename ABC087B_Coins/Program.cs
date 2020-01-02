549195dd59bba3a73de2120765de22049f80e19c
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-02 09:32:32 +0900

    ABC087B Solved

---
+++ ABC087B_Coins/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ABC087B_Coins
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            int cnt  = 0;
+            for (int i = 0; i <= a; i++)
+            {
+                for (int j = 0; j <= b; j++)
+                {
+                    for (int k = 0; k <= c; k++)
+                    {
+                        if(500*i + 100*j + 50*k == x)
+                        {
+                            cnt++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
