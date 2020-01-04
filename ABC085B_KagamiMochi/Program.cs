4bc48f429179a95e27c531b69dada113f5c3a41c
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-04 23:31:57 +0900

    ABC085B solved

---
+++ ABC085B_KagamiMochi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC085B_KagamiMochi
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> d = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                d.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine(d.Distinct().Count());
+
+        }
+    }
+}
