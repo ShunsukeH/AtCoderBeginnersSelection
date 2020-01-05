f07751a1b39a8ebb46f493ab5fe28c61eecd5eda
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-05 09:39:17 +0900

    ABC085C solved (too slow)

---
+++ ABC085C_Otoshidama/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace ABC085C_Otoshidama
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] inp = Console.ReadLine().Split(' ');
+            int n = int.Parse(inp[0]);
+            int y = int.Parse(inp[1]);
+
+            bool flag = false;
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n-i; j++)
+                {
+                    for (int k = 0; k <= n-i-j; k++)
+                    {
+                        //Console.WriteLine($"{i} {j} {k}");
+                        if (10000 * i + 5000 * j + 1000 * k == y && i+j+k == n)
+                        {
+
+                            flag = true;
+                            Console.WriteLine($"{i} {j} {k}");
+                            break;
+                        }
+                    }
+                    if (flag)
+                    {
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("-1 -1 -1");
+            }
+        }
+    }
+}
