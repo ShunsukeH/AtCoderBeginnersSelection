f766a7454ed3e16880f3c7fcdc1538249bf4c859
shunsuke hoshino <skhn7vp1cx4@gmail.com>
2020-01-05 10:03:36 +0900

    ABC085C modified

--- ABC085C_Otoshidama/Program.cs
+++ ABC085C_Otoshidama/Program.cs
@@ -12,21 +12,13 @@ namespace ABC085C_Otoshidama
             bool flag = false;
             for (int i = 0; i <= n; i++)
             {
-                for (int j = 0; j <= n-i; j++)
+                for (int j = 0; j <= n - i; j++)
                 {
-                    for (int k = 0; k <= n-i-j; k++)
+                    int k = n - (i + j);
+                    if (10000 * i + 5000 * j + 1000 * k == y)
                     {
-                        //Console.WriteLine($"{i} {j} {k}");
-                        if (10000 * i + 5000 * j + 1000 * k == y && i+j+k == n)
-                        {
-
-                            flag = true;
-                            Console.WriteLine($"{i} {j} {k}");
-                            break;
-                        }
-                    }
-                    if (flag)
-                    {
+                        flag = true;
+                        Console.WriteLine($"{i} {j} {k}");
                         break;
                     }
                 }
